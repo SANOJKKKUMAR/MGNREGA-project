bac34ff4f4874b173a6c322190c77990d811f6a0
sanoj kumar <146912742+SANOJKKKUMAR@users.noreply.github.com>
2025-10-22 11:29:30 +0530

    Update DistrictData.js

--- models/DistrictData.js
+++ models/DistrictData.js
@@ -1,8 +1,19 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('manrega', 'root', 'Sanoj@500r7', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+// const sequelize = new Sequelize('manrega', 'root', 'Sanoj@500r7', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+    }
+);
 
 const DistrictData = sequelize.define('DistrictData', {
     district: { type: DataTypes.STRING, allowNull: false },
